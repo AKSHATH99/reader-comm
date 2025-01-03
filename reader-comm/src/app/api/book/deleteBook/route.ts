0944d30558c48618e426c6def4a5ddbf93791d51
Akshath P <akshathpkk@gmail.com>
2025-01-03 10:30:38 +0530

    delete route

---
+++ reader-comm/src/app/api/book/deleteBook/route.ts
@@ -0,0 +1,31 @@
+import dbConnect from "@/lib/dbConnect";
+import BookModel from "@/model/Books";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  await dbConnect();
+
+  try {
+    const { BookID } = await request.json();
+    if (!BookID) {
+      return NextResponse.json({ message: "bookid not found" }, { status: 404 });
+    }
+
+    const deletedbook = await BookModel.findByIdAndDelete({ BookID });
+
+    if (deletedbook == null) {
+      return NextResponse.json(
+        { message: "Some error occured while deleting book" },
+        { status: 404 }
+      );
+    }
+
+    // const deleteBook = await BookModel.dele
+  } catch (error) {
+    console.error("Error during sign-in", error);
+    return NextResponse.json(
+      { message: "An error occurred during sign-in" },
+      { status: 500 }
+    );
+  }
+}
