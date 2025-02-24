ea93ce9942d85e321e8ce4400014f9821636275d
Akshath P <akshathpkk@gmail.com>
2025-02-24 21:07:44 +0530

    api to fetch all books

---
+++ reader-comm/src/app/api/book/all/route.ts
@@ -0,0 +1,37 @@
+//SEARCH FUNCTION FOR BOOK 
+
+import dbConnect from "@/lib/dbConnect";
+import BookModel from "@/model/Books";
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { slug: string } }
+) {
+    await dbConnect();
+
+    try {
+     
+        const bookDetails = await BookModel.find({}).select(
+          " BookName AuthorName PublishedDate BookCoverImage totalPages category Rating"
+        );
+
+        if(bookDetails.length==0 ){
+        return NextResponse.json("Books not found", { status: 200 });
+
+        }
+          
+        return NextResponse.json(bookDetails, { status: 200 });
+        
+
+    
+    } catch (error) {
+        console.error("Error during search", error);
+    return NextResponse.json(
+      { message: "An error occurred during search" },
+      { status: 500 }
+    );
+    }
+}
